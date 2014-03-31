794c5f59d7c91be920174eda4cfa17b3dc422fe8
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2014-03-31 10:25:24 +0200

    Added event handling example

--- view/js/core.js
+++ view/js/core.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+	/*
+	 * Setup window/webview
+	 */
 	PyInterface.execute('window', 'setWindowTitle', ['My Title']);
 
 	struct = [
@@ -17,4 +20,11 @@ $(document).ready(function() {
 	PyInterface.create_menu(JSON.stringify(struct));
 
 	PyInterface.execute('window', 'show', []);
+
+	/*
+	 * Setup event handling
+	 */
+	Events.on('func_name', function(e) {
+		alert('Event handled');
+	});
 });
